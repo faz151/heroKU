2aac629843e5a4739967d285686c31d0a8243565
faz151 <zagalfayeezullah@gmail.com>
2021-08-07 16:03:04 +0600

    mist

--- weather/forms.py
+++ weather/forms.py
@@ -6,7 +6,7 @@ from .models import City
 class CityForm(ModelForm):
     class Meta:
         model = City
-        fields = ['name']
+        fields = ['name','id']
         widgets = {'name': TextInput(
             attrs={'class': 'input', 'placeholder': 'City Name'})}
 
